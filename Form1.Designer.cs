5f174eb435580f37716f6dde99889ddbb1a333db
asus <asus@DESKTOP-7ND1U7G>
2023-10-07 17:59:11 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace terulet
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            textBox6 = new TextBox();
+            label3 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
+            textBox9 = new TextBox();
+            label4 = new Label();
+            textBox10 = new TextBox();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(302, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Kerület,terület";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton1.Location = new Point(76, 94);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(96, 29);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "négyzet";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton2.Location = new Point(561, 85);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(98, 29);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "téglalap";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton3.Location = new Point(320, 94);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(57, 29);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "kör";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(28, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 30);
+            label2.TabIndex = 4;
+            label2.Text = "oldal:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(116, 137);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(98, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(12, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 40);
+            button1.TabIndex = 6;
+            button1.Text = "Kerület:";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(12, 241);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 40);
+            button2.TabIndex = 7;
+            button2.Text = "Terület:";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(116, 254);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(98, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(116, 194);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(98, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(362, 194);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(98, 23);
+            textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(362, 254);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(98, 23);
+            textBox5.TabIndex = 14;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(258, 241);
+            button3.Name = "button3";
+            button3.Size = new Size(93, 40);
+            button3.TabIndex = 13;
+            button3.Text = "Terület:";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(258, 177);
+            button4.Name = "button4";
+            button4.Size = new Size(93, 40);
+            button4.TabIndex = 12;
+            button4.Text = "Kerület:";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(362, 137);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(98, 23);
+            textBox6.TabIndex = 11;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(274, 137);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 30);
+            label3.TabIndex = 10;
+            label3.Text = "sugár:";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(597, 217);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(98, 23);
+            textBox7.TabIndex = 21;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(597, 267);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(98, 23);
+            textBox8.TabIndex = 20;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(483, 254);
+            button5.Name = "button5";
+            button5.Size = new Size(93, 40);
+            button5.TabIndex = 19;
+            button5.Text = "Terület:";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(483, 200);
+            button6.Name = "button6";
+            button6.Size = new Size(93, 40);
+            button6.TabIndex = 18;
+            button6.Text = "Kerület:";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(597, 137);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(98, 23);
+            textBox9.TabIndex = 17;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(502, 132);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 25);
+            label4.TabIndex = 16;
+            label4.Text = "oldal:";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(597, 174);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(98, 23);
+            textBox10.TabIndex = 23;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(502, 167);
+            label5.Name = "label5";
+            label5.Size = new Size(68, 25);
+            label5.TabIndex = 22;
+            label5.Text = "oldal2:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox10);
+            Controls.Add(label5);
+            Controls.Add(textBox7);
+            Controls.Add(textBox8);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(textBox9);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(textBox6);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox6;
+        private Label label3;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button button5;
+        private Button button6;
+        private TextBox textBox9;
+        private Label label4;
+        private TextBox textBox10;
+        private Label label5;
+    }
+}
